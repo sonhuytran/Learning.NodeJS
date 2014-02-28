9fadb342cf8c99913d1f34ce43030baa3c7a92c8
Son-Huy TRAN <sonhuytran@gmail.com>
2014-02-28 17:33:11 +0100

    Try blocking & non-blocking functions

--- Books/TNBB/NodeJSHelloWorld/requestHandlers.js
+++ Books/TNBB/NodeJSHelloWorld/requestHandlers.js
@@ -5,8 +5,17 @@
 /**
  * Handles "start" request
  */
+var exec = require("child_process").exec;
+
 function start() {
-	console.log("Request handler 'start' was called.");
+	console.log("Request handler 'start' was called.");	
+	var content = "empty";
+	
+	exec("ls -lah", function(error, stdout, stderr) {
+		content = stdout;
+	});
+	
+	return content;
 }
 
 /**
@@ -14,6 +23,7 @@ function start() {
  */
 function upload() {
 	console.log("Request handler 'upload' was called.");
+	return "Hello Upload !!!";
 }
 
 // Exports series
--- Books/TNBB/NodeJSHelloWorld/router.js
+++ Books/TNBB/NodeJSHelloWorld/router.js
@@ -6,9 +6,10 @@ function route(handle, pathname) {
 	console.log("About to route a request for " + pathname);
 	
 	if (typeof(handle[pathname]) === 'function') {
-		handle[pathname]();
+		return handle[pathname]();
 	} else {
 		console.log("No request handler found for " + pathname);
+		return "404 Not found";
 	}
 }
 
