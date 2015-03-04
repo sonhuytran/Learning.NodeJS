cbbbbe1aebce3fea110b9126b03ea89800114899
sonhuytran <sonhuytran@gmail.com>
2015-03-04 17:43:51 +0100

    angular to-do 1

---
+++ angular/client/02/todo/todo-app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by stran on 04/03/2015.
+ */
+
+var model = {
+    user: "Son-Huy",
+    items: [
+        {action: "Buy Flowers", done: false},
+        {action: "Get Shoes", done: false},
+        {action: "Collect Tickets", done: true},
+        {action: "Call Joe", done: false}
+    ]
+};
+
+var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('ToDoController', ['$scope', function ($scope) {
+    $scope.todo = model;
+
+    $scope.incompleteCount = function () {
+        var count = 0;
+
+        angular.forEach($scope.todo.items, function (item) {
+            if (!item.done) {
+                count++;
+            }
+        });
+
+        return count;
+    };
+
+    $scope.warningLevel = function () {
+        return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    }
+}]);
