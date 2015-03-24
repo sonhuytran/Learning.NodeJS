dfb92cb8bff1614d0c410bce0a5461732856e454
sonhuytran <sonhuytran@gmail.com>
2015-03-24 13:25:29 +0100

    remove unused variables

--- angular/from-novice-to-ninja/01-test/app/test-app/test-app.js
+++ angular/from-novice-to-ninja/01-test/app/test-app/test-app.js
@@ -1,5 +1,5 @@
 /**
  * Created by stran on 23/03/2015.
  */
-var controllers = angular.module('testApp.controllers', []);
-var testApp = angular.module('testApp', ['testApp.controllers']);
+angular.module('testApp.controllers', []);
+angular.module('testApp', ['testApp.controllers']);
--- angular/from-novice-to-ninja/01-test/app/test-app/test-controllers.js
+++ angular/from-novice-to-ninja/01-test/app/test-app/test-controllers.js
@@ -1,10 +1,11 @@
 /**
  * Created by stran on 23/03/2015.
  */
-controllers.controller('FinanceController', ['$scope', function ($scope) {
-    $scope.salary = 1000;
-    $scope.percentage = 10;
-    $scope.result = function () {
-        return $scope.salary * $scope.percentage * 0.01;
-    }
-}]);
+angular.module('testApp.controllers')
+    .controller('FinanceController', ['$scope', function ($scope) {
+        $scope.salary = 1000;
+        $scope.percentage = 10;
+        $scope.result = function () {
+            return $scope.salary * $scope.percentage * 0.01;
+        }
+    }]);
